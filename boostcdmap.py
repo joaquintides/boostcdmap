ea86b3a887a498551a0fafa033fefb3c448193dc
joaquintides <joaquin@tid.es>
2020-06-02 13:22:10 +0200

    initial setup

---
+++ boostcdmap.py
@@ -0,0 +1,12 @@
+# boostcdmap: Boost conditional dependency map generator
+#
+# Copyright 2020 Joaquin M Lopez Munoz.
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+#
+# See https://github.com/joaquintides/boostcdmap/ for project home page.
+
+import sys
+
+sys.stdout.write("hello world\n")
