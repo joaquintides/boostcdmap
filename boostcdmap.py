404cd269c9de520d3ada7137cdd849f9e91fc71e
joaquintides <joaquin@tid.es>
2020-06-02 19:15:54 +0200

    changed project home page references

--- boostcdmap.py
+++ boostcdmap.py
@@ -5,7 +5,7 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 #
-# See https://github.com/joaquintides/boost_epoch/ for project home page.
+# See https://github.com/joaquintides/boostcdmap/ for project home page.
 
 import argparse
 import json
